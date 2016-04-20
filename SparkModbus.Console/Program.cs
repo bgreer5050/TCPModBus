e8b066353013d4858cee91a548cb014d234c49e5
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-04-20 09:32:15 -0500

    I cannot seem to change anything on the PLC

--- SparkModbus.Console/Program.cs
+++ SparkModbus.Console/Program.cs
@@ -3,40 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SRModbusTCP;
+//using SRModbusTCP;
+using EasyModbus;
 
 namespace SparkModbus.Console
 {
     class Program
     {
 
-        private  string ipAddress = "192.168.1.3";
-        private  int port = 502;
-        private ushort startAddress = 0;
-        private ushort qty = 10;
-        private  ModbusTCP modbusTCP;
+       
 
 
         public Program()
         {
-            modbusTCP = new ModbusTCP(ipAddress, port);
-            modbusTCP.Connect();
-            //int[] response = modbusTCP.ReadInputRegisters(startAddress, qty);
-            bool[] response = modbusTCP.ReadDiscreteInputs(40001, qty);
-          
-
-            modbusTCP.Disconnect();
-
-            System.Console.WriteLine("Reg 1: " + response[0].ToString());
-            System.Console.WriteLine("Reg 2: " + response[1].ToString());
-            System.Console.WriteLine("Reg 1: " + response[2].ToString());
-            System.Console.WriteLine("Reg 1: " + response[3].ToString());
-            System.Console.WriteLine("Reg 1: " + response[4].ToString());
-            System.Console.WriteLine("Reg 1: " + response[5].ToString());
-            System.Console.WriteLine("Reg 1: " + response[6].ToString());
-            System.Console.WriteLine("Reg 1: " + response[7].ToString());
-            System.Console.WriteLine("Reg 1: " + response[8].ToString());
-            System.Console.WriteLine("Reg 1: " + response[9].ToString());
+           
 
         }
 
@@ -44,8 +24,28 @@ namespace SparkModbus.Console
         static void Main(string[] args)
         {
 
-            Program program = new Program();
+            ModbusClient client = new ModbusClient("192.168.1.3", 502);
+            client.Connect();
+
+            for(var x=0; x < 1000; x++)
+            {
+                client.WriteSingleCoil(x, true);
+                System.Threading.Thread.Sleep(250);
+                System.Console.WriteLine(x.ToString());
+            }
+
+            for (var x = 0; x < 1000; x++)
+            {
+                client.WriteSingleCoil(x, false);
+                System.Threading.Thread.Sleep(250);
+
+            }
+
+
+
             System.Console.ReadKey();
+
+
 
 
 
