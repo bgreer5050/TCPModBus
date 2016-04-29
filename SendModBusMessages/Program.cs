d0bf824f11a4b26dd443c70c2690078e86ba9066
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-04-29 18:36:58 -0500

    jsnfjnfjernernjgnerjgn

--- SendModBusMessages/Program.cs
+++ SendModBusMessages/Program.cs
@@ -10,24 +10,37 @@ namespace SendModBusMessages
     {
         static void Main(string[] args)
         {
-           // EasyModbus.ModbusClient client = new EasyModbus.ModbusClient("127.0.0.1", 502);
+            // EasyModbus.ModbusClient client = new EasyModbus.ModbusClient("127.0.0.1", 502);
             //client.Connect();
 
-            for(var x=0; x < 10; x++)
-            {
-               EasyModbus.ModbusClient client = new EasyModbus.ModbusClient("127.0.0.1", 502);
-                client.Connect();
-                client.WriteSingleRegister(x, 1000);
-                client.Disconnect();
-            }
 
+
+         //   WriteValuesToHoldingRegisters();
+          
            
            
 
             while(true)
             {
+                Console.ReadLine();
+                WriteValuesToHoldingRegisters();
 
             }
+        }
+
+        private static void WriteValuesToHoldingRegisters()
+        {
+            //Writing the following array will write the following values in the 
+            //click PLC 
+            // DS1(400001) = 944
+            // DS2(400002) = 1
+
+            EasyModbus.ModbusClient client = new EasyModbus.ModbusClient("10.0.0.181", 502);
+            client.Connect();
+            int[] values = { 944, 1, 1, 1, 1, 2, 1, 1, 1, 500,944,1080 };
+            client.WriteMultipleRegisters(1, values);
+            client.Disconnect();
+            bool[] inputs = client.ReadDiscreteInputs(1,10);
         }
     }
 }
